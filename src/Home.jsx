6f6b75e7910a4ed1ff7b79714a90a62c33fbf73b
rm94697 <rm94697@estudante.fieb.edu.br>
2025-05-21 11:22:44 +0000

    Grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 import poke from'./assets/pokeapi_256.png'
 import eevee from'./assets/eevee.jpg'
 import'./Home.css'
+import PokeGrid from './PokeGrid'
+import PokeFlex from './PokeFlex'
+
+
 
 const Home = () => {
 
@@ -8,31 +12,8 @@ const Home = () => {
 return (
  <div>
 
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-<img src={poke} />
-<img src={eevee}/>
-
+<PokeGrid />
+<PokeFlex />
  </div>
 
 
